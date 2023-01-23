3bc298d26c01ca11b99cf499b8e475f476b90cbc
iiHadi66 <44447663+iiHadi66@users.noreply.github.com>
2023-01-23 13:51:38 +0100

    push to git hub

---
+++ ToDoProject/ToDoApp/Data/Models/TaskModels.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoApp.Data.Models
+{
+    public class TaskModels
+    {
+        public string? TaskName { get; set; }
+
+        public bool IsComplete { get; set; }
+    }
+}
---
+++ ToDoProject/ToDoApp/Data/Models/ToDoTest.cs
@@ -0,0 +1,45 @@
+﻿using ToDoApp.Pages.Components;
+using Xunit;
+
+namespace ToDoApp.Data.Models
+{
+    public class ToDoTest
+    {
+        [Fact]
+        public void AddTask_ShouldAddTaskToList()
+        {
+            // Arrange
+            var task = new TaskModels { TaskName = "Test Task" };
+            var component = new ToDo();
+
+            // Act
+            component.AddTask();
+
+            // Assert
+            Assert.Single(component.taskList);
+            Assert.Equal("Test Task", component.taskList[0].TaskName);
+        }
+
+
+        [Fact]
+        public void PercentageCompletedShouldReturnCorrectPercentage()
+        {
+            // Arrange
+            var taskList = new List<TaskModels>
+    {
+        new TaskModels { TaskName = "Task 1", IsComplete = true },
+        new TaskModels { TaskName = "Task 2", IsComplete = false },
+        new TaskModels { TaskName = "Task 3", IsComplete = true }
+    };
+            var component = new ToDo();
+            component.taskList = taskList;
+            // Act
+            var percentage = component.PercentageCompleted;
+            // Assert
+            Assert.Equal(67, percentage);
+        }
+
+
+
+    }
+}
